06ef47ff8feb88bfb07cb3116a7e26ecc7f84207
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-08-21 14:44:54 +0300

    Update MainController.kt

--- src/main/kotlin/org/dynamium/etcalc/controller/MainController.kt
+++ src/main/kotlin/org/dynamium/etcalc/controller/MainController.kt
@@ -2,15 +2,21 @@ package org.dynamium.etcalc.controller
 
 import org.dynamium.etcalc.dataclasses.DefaultResponse
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MainController {
     @GetMapping("/")
-    fun homePageView(): DefaultResponse {
+    private fun homePageView(): DefaultResponse {
         return DefaultResponse(
                 text = "You have accessed the homepage of ETCalc service. To start working with ETCalc, please proceed to link specified here.",
                 link = "https://example.com"
         )
+    }
+
+    @GetMapping("/calculate/mileage/")
+    private fun calculateView() {
+
     }
 }
