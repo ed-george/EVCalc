e7bbf1d4f6de989c7da21983bb7f7a5835576a89
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-08-30 22:09:55 +0300

    add logging

--- src/main/kotlin/org/dynamium/etcalc/EtCalcApplication.kt
+++ src/main/kotlin/org/dynamium/etcalc/EtCalcApplication.kt
@@ -1,5 +1,7 @@
 package org.dynamium.etcalc
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +9,7 @@ import org.springframework.boot.runApplication
 class EtCalcApplication
 
 fun main(args: Array<String>) {
+    val logger: Logger = LoggerFactory.getLogger(EtCalcApplication::class.java)
     runApplication<EtCalcApplication>(*args)
+    logger.info("ETCalc application has successfully started!")
 }
--- src/main/kotlin/org/dynamium/etcalc/core/calculate/CalculateBatteryDrain.kt
+++ src/main/kotlin/org/dynamium/etcalc/core/calculate/CalculateBatteryDrain.kt
@@ -1,4 +1,8 @@
 package org.dynamium.etcalc.core.calculate
 
 object CalculateBatteryDrain {
+    fun calculateEUC(riderWeight: Int, chargeCycles: Int, currentPercentage: Int, batterySize: Int, speedType: String): Int {
+        val result = 0
+        return result
+    }
 }
