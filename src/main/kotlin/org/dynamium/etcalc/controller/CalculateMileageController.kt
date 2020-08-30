459af018f51f0e194cb44c66aa337dcfe9a4f16b
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-08-31 00:47:55 +0300

    Update CalculateMileageController.kt

--- src/main/kotlin/org/dynamium/etcalc/controller/CalculateMileageController.kt
+++ src/main/kotlin/org/dynamium/etcalc/controller/CalculateMileageController.kt
@@ -1,12 +1,16 @@
 package org.dynamium.etcalc.controller
 
 import org.dynamium.etcalc.core.calculate.CalculateMileage
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CalculateMileageController {
+
+    val logger: Logger = LoggerFactory.getLogger(CalculateMileageController::class.java)
 
     @GetMapping("/mileage/euc")
     private fun calculateMileageEUCView(
@@ -16,7 +20,11 @@ class CalculateMileageController {
             @RequestParam(value = "batterySize") batterySize: Int,
             @RequestParam(value = "speedType") speedType: String
     ): Int {
-        return CalculateMileage.calculateEUC(riderWeight, chargeCycles, batterySize, currentPercentage, speedType)
+        val result = CalculateMileage.calculateEUC(riderWeight, chargeCycles, batterySize, currentPercentage, speedType)
+        logger.info("Called /mileage/euc endpoint with arguments riderWeight: $riderWeight, " +
+                "chargeCycles: $chargeCycles, currentPercentage: $currentPercentage, batterySize: $batterySize, speedTYpe: $speedType. " +
+                "Returned value: $result")
+        return result
     }
 
     @GetMapping("/mileage/es")
