4a154af579c51c85095ba8f5814fec88d874b21a
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-10-16 19:52:52 +0300

    Add GitHub Packages test

--- evcalc-core/build.gradle.kts
+++ evcalc-core/build.gradle.kts
@@ -4,12 +4,32 @@ plugins {
     kotlin("jvm") version "1.4.10"
     id("maven-publish")
 }
-group = "org.dynamium.etcalc"
+group = "org.dynamium.evcalc"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Dynamium/EVCalc")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("EVCALC_GHP_USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("EVCALC_GHP_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("evcalc") {
+            from(components["java"])
+        }
+    }
+}
+
 dependencies {
     testImplementation(kotlin("test-junit"))
 }
